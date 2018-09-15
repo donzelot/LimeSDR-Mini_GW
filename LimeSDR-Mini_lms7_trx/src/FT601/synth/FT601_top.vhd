46fc99b643a1c16e8660d976aabd4e9e77835fc9
Unknown <gkarm@live.com>
2018-09-15 12:36:16 +0300

    Synthesisze

--- LimeSDR-Mini_lms7_trx/src/FT601/synth/FT601_top.vhd
+++ LimeSDR-Mini_lms7_trx/src/FT601/synth/FT601_top.vhd
@@ -1,10 +1,10 @@
--- ----------------------------------------------------------------------------	
+-- ----------------------------------------------------------------------------
 -- FILE: 	FT601_top.vhd
 -- DESCRIPTION:	top module for FT601
 -- DATE:	May 13, 2016
 -- AUTHOR(s):	Lime Microsystems
 -- REVISIONS:
--- ----------------------------------------------------------------------------	
+-- ----------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -14,18 +14,18 @@ use ieee.numeric_std.all;
 -- ----------------------------------------------------------------------------
 entity FT601_top is
 	generic(
-			EP02_rdusedw_width	: integer := 11; 
+			EP02_rdusedw_width	: integer := 11;
 			EP02_rwidth				: integer := 8;
 			EP82_wrusedw_width	: integer := 11;
 			EP82_wwidth				: integer := 8;
-			EP82_wsize  			: integer := 64;  --packet size in bytes, has to be multiple of 4 bytes				
+			EP82_wsize  			: integer := 64;  --packet size in bytes, has to be multiple of 4 bytes
 			EP03_rwidth				: integer := 32;
 			EP83_wwidth				: integer := 32;
 			EP83_fifo_rwidth : integer := 9;
 			EP83_wsize  			: integer := 512
 	);
 	port (
-			--input ports 
+			--input ports
 			clk         	: in std_logic;			--FTDI CLK
 			fft_clk			: in std_logic;
 			reset_n   		: in std_logic;
@@ -35,7 +35,7 @@ entity FT601_top is
 			FT_data			: inout std_logic_vector(31 downto 0);
 			FT_be				: inout std_logic_vector(3 downto 0);
 			FT_txe_n			: in std_logic;
-		   --controll endpoint fifo PC->FPGA 
+		   --controll endpoint fifo PC->FPGA
 			EP02_rdclk		: in std_logic;
 			EP02_rd			: in std_logic;
 			EP02_rdata		: out std_logic_vector(EP02_rwidth-1 downto 0);
@@ -61,7 +61,7 @@ entity FT601_top is
 			EP83_wdata		: in std_logic_vector(EP83_wwidth-1 downto 0);
 			EP83_wfull		: out std_logic;
 			EP83_wrusedw	: out std_logic_vector(11 downto 0)
-        
+
         );
 end FT601_top;
 
@@ -71,17 +71,17 @@ end FT601_top;
 architecture arch of FT601_top is
 --declare signals,  components here
 
---EP02 fifo signals 
+--EP02 fifo signals
 signal EP02_wrempty			: std_logic;
-signal EP02_wr					: std_logic; 
+signal EP02_wr					: std_logic;
 signal EP02_wdata				: std_logic_vector(31 downto 0);
 --EP82 fifo signals
 signal EP82_fifo_rdusedw	: std_logic_vector(EP82_wrusedw_width-1 downto 0);
 signal EP82_fifo_q			: std_logic_vector(31 downto 0);
 signal EP82_fifo_rdreq		: std_logic;
 
---EP03 fifo signals 
-signal EP03_wrempty			: std_logic; 
+--EP03 fifo signals
+signal EP03_wrempty			: std_logic;
 signal EP03_wr					: std_logic;
 signal EP03_wdata				: std_logic_vector(31 downto 0);
 
@@ -91,7 +91,7 @@ signal EP83_fifo_q			: std_logic_vector(31 downto 0);
 signal EP83_fifo_rdreq		: std_logic;
 
 --arbiter signals
-signal arb_en 					: std_logic; 
+signal arb_en 					: std_logic;
 signal arb_rd_wr				: std_logic;
 signal arb_nth_ch				: std_logic_vector(3 downto 0);
 
@@ -106,11 +106,11 @@ signal fsm_wr_data  			: std_logic_vector(31 downto 0);
 component fifo_inst is
   generic(dev_family	     : string  := "MAX 10";
           wrwidth         : integer := 64;
-          wrusedw_witdth  : integer := 9; --12=2048 words 
+          wrusedw_witdth  : integer := 9; --12=2048 words
           rdwidth         : integer := 32;
           rdusedw_width   : integer := 10;
           show_ahead      : string  := "OFF"
-  );  
+  );
 
   port (
 
@@ -125,21 +125,21 @@ component fifo_inst is
       rdreq         : in std_logic;
       q             : out std_logic_vector(rdwidth-1 downto 0);
       rdempty       : out std_logic;
-      rdusedw       : out std_logic_vector(rdusedw_width-1 downto 0)     
-        
+      rdusedw       : out std_logic_vector(rdusedw_width-1 downto 0)
+
         );
 end component;
 
 --FT6001 arbiter component
 component FT601_arb is
-	generic(	
+	generic(
 			EP82_fifo_rwidth	: integer := 9;
 			EP82_wsize       	: integer := 64;  --packet size in bytes, has to be multiple of 4 bytes
 			EP83_fifo_rwidth	: integer := EP83_fifo_rwidth;
 			EP83_wsize       	: integer := EP83_wsize --packet size in bytes, has to be multiple of 4 bytes
 	);
   port (
-			--input ports 
+			--input ports
 			clk       			: in std_logic;
 			reset_n   			: in std_logic;
 			enable       		: in std_logic;
@@ -154,7 +154,7 @@ component FT601_arb is
 			--stream EP PC->FPGA
 			EP03_fifo_data		: out std_logic_vector(31 downto 0);
 			EP03_fifo_wr		: out std_logic;
-			EP03_fifo_wrempty	: in std_logic;		
+			EP03_fifo_wrempty	: in std_logic;
 			--stream EP FPGA->PC
 			EP83_fifo_data		: in std_logic_vector(31 downto 0);
 			EP83_fifo_rd 		: out std_logic;
@@ -168,13 +168,13 @@ component FT601_arb is
          fsm_rddata        : in std_logic_vector(31 downto 0);
          fsm_wrdata_req    : in std_logic;
          fsm_wrdata        : out std_logic_vector(31 downto 0);
-			
+
 			ep_status			: in std_logic_vector(7 downto 0)
-        
+
         );
 end component;
 
---FT601 fsm component 
+--FT601 fsm component
 component FT601 is
     generic(
 			EP82_wsize       : integer := 64;  	--packet size in bytes, has to be multiple of 4 bytes
@@ -189,8 +189,8 @@ component FT601 is
 			ch_n     	   : in std_logic_vector(3 downto 0);
          RD_data_valid  : out std_logic;
 			RD_data        : out std_logic_vector(31 downto 0);
-         WR_data_req    : out std_logic;     
-			WR_data        : in std_logic_vector(31 downto 0); -- should be 2 cycle latency after WR_data_req   
+         WR_data_req    : out std_logic;
+			WR_data        : in std_logic_vector(31 downto 0); -- should be 2 cycle latency after WR_data_req
 			wr_n			   : out std_logic;
 			rxf_n			   : in std_logic;
 			data			   : inout std_logic_vector(31 downto 0);
@@ -199,30 +199,25 @@ component FT601 is
         );
 end component;
 
-
- 
---  signal real_a, imag_a, real_a_final, imag_a_final: std_logic_vector(15 downto 0);
-  signal real_a, imag_a, real_a_final, imag_a_final: std_logic_vector(17 downto 0);
-  signal fft_out :std_logic_vector (68 downto 0);
-  signal package_start: std_logic;
+  signal fft_out :std_logic_vector(32 downto 0);
 begin
 
 
 -- ----------------------------------------------------------------------------
 -- FTDI endpoint fifos
 -- ----------------------------------------------------------------------------
--- controll PC->FPGA 
-EP02_fifo : fifo_inst		
+-- controll PC->FPGA
+EP02_fifo : fifo_inst
 generic map(
 		dev_family		=> "Cyclone IV",
-		wrwidth			=> 32,								--32 bits ftdi side, 
+		wrwidth			=> 32,								--32 bits ftdi side,
 		wrusedw_witdth	=> EP02_rdusedw_width, 		--10=512 words (2048kB)
 		rdwidth			=> EP02_rwidth,
-		rdusedw_width	=> EP02_rdusedw_width,				
+		rdusedw_width	=> EP02_rdusedw_width,
 		show_ahead     => "OFF"
 )
 port map(
-      reset_n       	=> reset_n, 
+      reset_n       	=> reset_n,
       wrclk         	=> clk,
       wrreq         	=> EP02_wr,
       data          	=> EP02_wdata,
@@ -233,21 +228,21 @@ port map(
       rdreq         	=> EP02_rd,
       q             	=> EP02_rdata,
       rdempty       	=> EP02_rempty,
-      rdusedw       	=> open             
+      rdusedw       	=> open
         );
 
 -- controll FPGA->PC
-EP82_fifo : fifo_inst		
+EP82_fifo : fifo_inst
 generic map(
 		dev_family		=> "Cyclone IV",
 		wrwidth			=> EP82_wwidth,
 		wrusedw_witdth	=> EP82_wrusedw_width, 						--12=2048 words (2048kB)
-		rdwidth			=> 32,						--32 bits ftdi side, 
-		rdusedw_width	=> EP82_wrusedw_width,				
+		rdwidth			=> 32,						--32 bits ftdi side,
+		rdusedw_width	=> EP82_wrusedw_width,
 		show_ahead		=> "ON"
 )
 port map(
-      reset_n       	=> EP82_aclrn, 
+      reset_n       	=> EP82_aclrn,
       wrclk				=> EP82_wclk,
       wrreq				=> EP82_wr,
       data          	=> EP82_wdata,
@@ -258,81 +253,37 @@ port map(
       rdreq         	=> EP82_fifo_rdreq,
       q             	=> EP82_fifo_q,
       rdempty       	=> open,
-      rdusedw       	=> EP82_fifo_rdusedw           
+      rdusedw       	=> EP82_fifo_rdusedw
         );
 
 
-		  
-		  
---stream PC->FPGA		
-
-
--- IQ register with 2x clock rate
-process(EP83_wclk)
-begin 
-	if (EP83_wclk'event AND EP83_wclk='1') then
-		real_a <= std_logic_vector(shift_left(resize(signed(EP83_wdata(63 downto 52)), 18), 6));
-		imag_a <= std_logic_vector(shift_left(resize(signed(EP83_wdata(51 downto 40)), 18), 6));
-	end if;
-end process;	
-
-
-
---EP03_fifo : fifo_inst		
---generic map(
---		dev_family		=> "Cyclone IV",
---		wrwidth			=> 32,						--32 bits ftdi side, 
---		wrusedw_witdth	=> 10, 						--10=512 words (2048kB)
---		rdwidth			=> EP03_rwidth,
---		rdusedw_width	=> 10,				
---		show_ahead     => "OFF"
---)
---port map(
---      reset_n       	=> reset_n, 
---      wrclk         	=> clk,
---      wrreq         	=> EP03_wr,
---      data          	=> EP03_wdata,
---      wrfull        	=> open,
---		wrempty		  	=> EP03_wrempty,
---      wrusedw       	=> open,
---      rdclk 	     	=> EP03_rdclk,
---      rdreq         	=> EP03_rd,
---      q             	=> EP03_rdata,
---      rdempty       	=> EP03_rempty,
---      rdusedw       	=> open             
---        );	
-	
-
-	
 fft: entity work.top
     port map (
         clk       => fft_clk,
         rst_n   	=> '1',
-		  
+
         -- inputs
-        in0 => real_a & imag_a,
+        in0 => '1' & EP83_wdata(63 downto 40),
 
         -- outputs
         out0 => fft_out
     );
 
-
-package_start <= '1' when to_integer(signed(fft_out(67 downto 36))) = 0 else '0';
 -- stream FPGA->PC
-EP83_fifo : fifo_inst		
+EP83_fifo : fifo_inst
 generic map(
 		dev_family		=> "Cyclone IV",
 		wrwidth			=> 32,
 		wrusedw_witdth	=> EP83_fifo_rwidth, 			--11=1024 words x EP83_wwidth (8192KB)
-		rdwidth			=> 32,			--32 bits ftdi side, 
-		rdusedw_width	=> EP83_fifo_rwidth,				
+		rdwidth			=> 32,			--32 bits ftdi side,
+		rdusedw_width	=> EP83_fifo_rwidth,
 		show_ahead		=> "ON"
 )
 port map(
-      reset_n       	=> EP83_aclrn, 
+      reset_n       	=> EP83_aclrn,
       wrclk				=> fft_clk,
-      wrreq        => fft_out(68), 
-      data            => fft_out(30 downto 0) & package_start, 
+      wrreq        => fft_out(32),
+      data            => fft_out(31 downto 0),
       wrfull        	=> open,
 		wrempty		  	=> open,
       wrusedw       	=> open,
@@ -340,75 +291,25 @@ port map(
       rdreq         	=> EP83_fifo_rdreq,
       q             	=> EP83_fifo_q,
       rdempty       	=> open,
-      rdusedw       	=> EP83_fifo_rdusedw           
+      rdusedw       	=> EP83_fifo_rdusedw
 		);
-		 
 
----- IQ register with 2x clock rate
---process(EP83_wclk)
---begin 
---	if (EP83_wclk'event AND EP83_wclk='1') then
---		real_a <= real_a;
---		imag_a <= imag_a;
---		if EP83_wr = '1' then
---			real_a <= std_logic_vector(shift_left(resize(signed(EP83_wdata(63 downto 52)), 16), 4));
---			imag_a <= std_logic_vector(shift_left(resize(signed(EP83_wdata(51 downto 40)), 16), 4));
---		end if;
---	end if;
---end process;	
---
----- downsample by 2
---process(fft_clk)
---begin 
---	if (fft_clk'event AND fft_clk='1') then
---		real_a_final <= real_a;
---		imag_a_final <= imag_a;
---	end if;
---end process;	
---
---
---	
----- stream FPGA->PC
---EP83_fifo : fifo_inst		
---generic map(
---		dev_family		=> "Cyclone IV",
---		wrwidth			=> 32,
---		wrusedw_witdth	=> EP83_fifo_rwidth, 			--11=1024 words x EP83_wwidth (8192KB)
---		rdwidth			=> 32,			--32 bits ftdi side, 
---		rdusedw_width	=> EP83_fifo_rwidth,				
---		show_ahead		=> "ON"
---)
---port map(
---      reset_n       	=> EP83_aclrn, 
---      wrclk				=> fft_clk,
---      wrreq				=> '1',
---      data          	=> real_a_final & imag_a_final,
---      wrfull        	=> open,
---		wrempty		  	=> open,
---      wrusedw       	=> open,
---      rdclk 	     	=> clk,
---      rdreq         	=> EP83_fifo_rdreq,
---      q             	=> EP83_fifo_q,
---      rdempty       	=> open,
---      rdusedw       	=> EP83_fifo_rdusedw           
---		);
-		 
 -- ----------------------------------------------------------------------------
 -- FTDI arbiter
--- ----------------------------------------------------------------------------		
+-- ----------------------------------------------------------------------------
 	ftdi_arbiter : FT601_arb
-	generic map(	
+	generic map(
 			EP82_fifo_rwidth	=> EP82_wrusedw_width,
 			EP82_wsize       	=> EP82_wsize,
 			EP83_fifo_rwidth	=> EP83_fifo_rwidth,
 			EP83_wsize       	=> EP83_wsize
 	)
   port map(
-			clk       			=> clk, 
+			clk       			=> clk,
 			reset_n   			=> reset_n,
 			enable       		=> '1',
-			EP02_fifo_data		=> EP02_wdata, 
-			EP02_fifo_wr		=> EP02_wr, 
+			EP02_fifo_data		=> EP02_wdata,
+			EP02_fifo_wr		=> EP02_wr,
 			EP02_fifo_wrempty	=> EP02_wrempty,
 			EP82_fifo_data		=> EP82_fifo_q,
 			EP82_fifo_rd 		=> EP82_fifo_rdreq,
@@ -417,30 +318,30 @@ port map(
 			EP03_fifo_wr		=> EP03_wr,
 			EP03_fifo_wrempty	=> ext_buff_rdy, --EP03_wrempty, fifo is not used
 			EP83_fifo_data		=> EP83_fifo_q,
-			EP83_fifo_rd 		=> EP83_fifo_rdreq,	
+			EP83_fifo_rd 		=> EP83_fifo_rdreq,
 			EP83_fifo_rdusedw	=> EP83_fifo_rdusedw,
 
-			fsm_epgo				=> arb_en, 
+			fsm_epgo				=> arb_en,
 			fsm_rdwr				=> arb_rd_wr,
-			fsm_ch				=> arb_nth_ch, 		 
-			fsm_rdy				=> fsm_rdy, 
+			fsm_ch				=> arb_nth_ch,
+			fsm_rdy				=> fsm_rdy,
 			fsm_rddata_valid	=> fsm_rd_data_valid,
 			fsm_rddata			=> fsm_rd_data,
          fsm_wrdata_req    => fsm_wr_data_req,
 			fsm_wrdata			=> fsm_wr_data,
-			ep_status			=> FT_data(15 downto 8)       
+			ep_status			=> FT_data(15 downto 8)
 			);
-			
-ext_buff_data	<=EP03_wdata;	
-ext_buff_wr		<=EP03_wr; 		
-		  
+
+ext_buff_data	<=EP03_wdata;
+ext_buff_wr		<=EP03_wr;
+
 -- ----------------------------------------------------------------------------
--- FTDI fsm 
--- ----------------------------------------------------------------------------		  
+-- FTDI fsm
+-- ----------------------------------------------------------------------------
 ft_fsm : FT601
 generic map(
 			EP82_wsize	=> EP82_wsize,
-			EP83_wsize	=> EP83_wsize 
+			EP83_wsize	=> EP83_wsize
 			)
   port map (
 			clk			   => clk,
@@ -449,21 +350,16 @@ generic map(
 			ready			   => fsm_rdy,
 			rd_wr    	   => arb_rd_wr,
 			ch_n     	   => arb_nth_ch,
-			RD_data_valid	=> fsm_rd_data_valid, 
+			RD_data_valid	=> fsm_rd_data_valid,
 			RD_data   	   => fsm_rd_data,
          WR_data_req    => fsm_wr_data_req,
-			WR_data  	   => fsm_wr_data,  
+			WR_data  	   => fsm_wr_data,
 			wr_n			   => FT_wr_n,
 			rxf_n			   => FT_rxf_n,
 			data			   => FT_data,
 			be				   => FT_be,
 			txe_n			   => FT_txe_n
 			);
-		  
-  
+
+
 end arch;
-
-
-
-
-
